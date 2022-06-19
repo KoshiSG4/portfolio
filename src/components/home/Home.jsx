86413ea3ebfe592b66893626e515c527d3713a2e
Koshi <koshila.20200695@iit.ac.lk>
2022-06-19 19:55:55 +0530

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,20 +11,8 @@ export default function Home() {
         <div className="left">
           <h2>Hi There!</h2>
           <h1><span>Koshi</span> here.</h1>
-          {/* <h3> I'm Interested In&nbsp;
-            <span>
-              <Typewriter
-                options={{ 
-                  strings:["Full-Stack Development", "Web Development", "Mobile App Development","Human Computer Interactions"],
-                  autoStart:true,
-                  delay:50,
-                  loop:true
-                  }}
-              />
-            </span>
-          </h3> */}
           <div className="typing">
-            <h3> I'm Interested In&nbsp;</h3>
+            <h3> I'm Interested In,&nbsp;</h3>
             <h3>
               <Typewriter
                 options={{ 
