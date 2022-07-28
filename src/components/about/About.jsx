eaebc37af396860edfff7f2fd53f63c318c954f8
Koshi <koshila.20200695@iit.ac.lk>
2022-07-28 13:06:45 +0530

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                   <h1>About Me</h1>
                   <p>Hello! My name is Koshila Gunasinghe and I am a second year computer science student at IIT Sri Lanka which is awarded by the University of Westminster in the UK.</p>
                   <p> I enjoy learning, experimenting and building new programms. My main focus is full-stack web & mobile development which allows me to use while improving my creativity along with the analytical skills. Also, I am interested in machine learning concepts. </p>
-                  <p>In addition to my academic and career interests, I enjoy listening to music, mostly pop music, watching tv series, sometimes reading fictions and plant veggies in our garden.</p>
+                  <p>In addition to my academic and career interests, I enjoy listening to music, mostly pop music, watching tv series, sometimes reading fictions and planting veggies in our garden.</p>
                 </div> 
             </div>
         </div>
